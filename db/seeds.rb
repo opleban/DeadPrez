c7527f0008c319045e6802e578e55a8dbdfd2e02
Alex Birdsall <ambirdsall@gmail.com>
2014-07-31 22:14:58 -0700

    adds functional seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 require 'csv'
+require_relative '../config/application'
+require_relative '../app/models/presidents'
+require_relative '../app/models/quotes'
 
-CSV.foreach("../../prezquotes.csv") do |line|
-  president = President.create(name: line[1])
+CSV.foreach("/Users/alexbirdsall/DBC/DeadPrez/prezquotes.csv") do |line|
+  if President.where('name=?', line[1]).empty?
+    president = President.create(name: line[1])
+  else
+    president = President.where('name=?', line[1])[0]
+  end
   quote = Quote.create(quote: line[0])
-  quote.president = president
+  president.quotes << quote
 end
