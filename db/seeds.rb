672d277e99d4a4d3ff7631dedbcac2ebeef5d02f
Alex Birdsall <ambirdsall@gmail.com>
2014-07-31 22:55:55 -0700

    fixes filepath in seed file; db prepped but unseeded.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@ require_relative '../config/application'
 require_relative '../app/models/presidents'
 require_relative '../app/models/quotes'
 
-CSV.foreach("/Users/alexbirdsall/DBC/DeadPrez/prezquotes.csv") do |line|
+csv_path = APP_ROOT.join("prezquotes.csv").to_s
+
+CSV.foreach(csv_path) do |line|
   if President.where('name=?', line[1]).empty?
     president = President.create(name: line[1])
   else
